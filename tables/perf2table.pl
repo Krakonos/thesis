f58100b76c352279545d034721e85b222c977cca
Ladislav Láska <krakonos@krakonos.org>
2017-01-01 22:13:26 +0100

    Added perf table generator.

---
+++ tables/perf2table.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use utf8;
+use common::sense;
+
+my $c = 20;
+
+print "\\begin{tabular}{|c|l|l|l|}";
+print "\\hline Overhead & Command & Sh. Obj. & Symbol \\\\  \\hline\n\\hline";
+while (<>) {
+	next if /^#/;
+	last if ($c-- == 0);
+	chomp;
+	s/ +/ /g;
+	s/%/\\%/g;
+	s/_/\\_/g;
+	s/lto1-//;
+	s/-....\.so//;
+	my @a = split / +/,$_,6;
+	print join(" & ", "\$$a[1]\$", "\\tt $a[2]", "\\tt $a[3]", "\\tt $a[5]")."\\\\\n\\hline ";
+}
+print "\\end{tabular}";
